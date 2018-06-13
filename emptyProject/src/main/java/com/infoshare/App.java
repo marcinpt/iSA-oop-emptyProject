6659aca4e9525770b55ad6c5e461c9a579c3e276
Marcin Piatek <marcinpt@onet.pl>
2018-06-13 17:05:40 +0900

    cw-4-1

--- emptyProject/src/main/java/com/infoshare/App.java
+++ emptyProject/src/main/java/com/infoshare/App.java
@@ -6,7 +6,8 @@ package com.infoshare;
  */
 public class App 
 {
-//    public static String hello(String imie) {
+// Cw 1
+// public static String hello(String imie) {
 //        return imie;
 //    }
 //
@@ -20,13 +21,21 @@ public class App
 
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
+//
+// System.out.println( "Hello World!" );
 //        System.out.println("Witaj " + hello("Marcin"));
 //        System.out.println("Imie odwrotnie: " + imieOdwrotnie("Marcin"));
 
-        Pies burek = new Pies("bokser", 20, "Azor", 20);
-        System.out.println(burek.szczekaj());
-        System.out.println("BMI wynosi: " + burek.oobliczBmi(20, 20));
+//        Pies burek = new Pies("bokser", 20, "Azor", 20);
+//        System.out.println(burek.szczekaj());
+//        System.out.println("BMI wynosi: " + burek.oobliczBmi(20, 20));
+
+        Pies p1 = new Pies();
+        Pies p2 = new Pies("jamnik", 100, "Burek" );
+        System.out.println(p1.szczekaj());
+        System.out.println(p2.szczekaj());
+
+
     }
 }
 
--- emptyProject/src/main/java/com/infoshare/Pies.java
+++ emptyProject/src/main/java/com/infoshare/Pies.java
@@ -6,17 +6,26 @@ public class Pies {
     public String imie;
     public Integer wzrost;
 
+    public Pies(String rasa, Integer waga, String imie) {
+        this(rasa, waga, imie, 0);
+    }
     public Pies(String rasa, Integer waga, String imie, Integer wzrost) {
         this.rasa = rasa;
         this.waga = waga;
         this.imie = imie;
         this.wzrost = wzrost;
     }
-    public String szczekaj() {
-        return rasa + " " + imie + " o wadze " + waga + " kg zaszczekal";
+    public Pies() {
+        this("jamnik", 100, "Szarik", 180);
     }
 
-    public double oobliczBmi(Integer waga, Integer wzrost) {
-        return waga / wzrost * wzrost * 1.0;
+    public String szczekaj() {
+        return rasa + " " + imie + " o wadze " + waga + " kg, wzroscie " + wzrost + " zrobil hau hau";
     }
-}
+
+
+
+//    public double oobliczBmi(Integer waga, Integer wzrost) {
+//        return waga / (wzrost * wzrost) * 1.0;
+    }
+
